0df857b60348134b7c7baa56c15d118447d40b28
AlexDDks <magarciaa92@gmail.com>
2022-05-17 20:30:11 -0500

    subiendo login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,12 @@
 const controller = {
     login: (req, res) => {
         res.render('login')
+    },
+
+    pruebas: (req, res) => {
+        res.render('pruebas')
     }
 }
+
 
 module.exports = controller;
